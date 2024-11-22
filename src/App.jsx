78fd7860bf5d9b97a1799a37dcaf59be54549a0b
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2024-11-22 14:34:01 +0530

    project initiated on 21nov 2024

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <>
+    // <div className="bg-gray-300 mb-8">
+    //   <h1>Hello World!</h1> 
+    // </div>
+
+    // <div className="mb-8 bg-slate-600">
+    //   <h1>Hello World!</h1> 
+    // </div>
+
+    // <div className="bg-orange-400 p-8 m-8" >
+    //   <p>Home</p>
+    //   <p>Contacts</p>
+    //   <p>Demo</p> 
+    // </div>
+
+    // <div className='bg-indigo-700 p-20 m-10'>
+    //   <h1 className='text-center text-white text-4xl'>Welcome Home</h1>
+    // </div>
+    // </>
+
+    <>
+      <div className='bg-orange-500 text-white p-[265px]'>
+        <h1 className='text-center text-5xl tracking-widest'>NETFLIX</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
